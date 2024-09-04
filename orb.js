4913cea35f12cbddf20ced7e2e2474a7c4395791
Yaroslav Avtonomov <avtnmv@icloud.com>
2024-09-04 22:13:34 +0300

    feat: update colors for light theme

--- orb.js
+++ orb.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container');
-for (let i = 0; i < 400; i++) {
+for (let i = 0; i < 600; i++) {
     const div = document.createElement('div');
     div.className = 'c';
     container.appendChild(div);
