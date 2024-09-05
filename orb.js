5842ab2ae75c39874ef1aea12803bd0a17fd18aa
Yaroslav Avtonomov <avtnmv@icloud.com>
2024-09-05 16:43:02 +0300

    feat: update slider.js & orb

--- orb.js
+++ orb.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container');
-for (let i = 0; i < 300; i++) {
+for (let i = 0; i < 270; i++) {
     const div = document.createElement('div');
     div.className = 'c';
     container.appendChild(div);
--- slider.js
+++ slider.js
@@ -1,10 +1,35 @@
 var swiper = new Swiper('.mySwiper', {
   navigation: {
-    nextEl: '.custom-next',
-    prevEl: '.custom-prev',
+      nextEl: '.custom-next',
+      prevEl: '.custom-prev',
   },
   pagination: {
-    el: '.custom-swiper-pagination',
-    type: 'progressbar',
+      el: '.custom-swiper-pagination',
+      type: 'progressbar',
   },
+  on: {
+      init: function() {
+          updateNavButtons(this);
+      },
+      slideChange: function() {
+          updateNavButtons(this);
+      }
+  }
 });
+
+function updateNavButtons(swiper) {
+  const prevButton = document.querySelector('.custom-prev');
+  const nextButton = document.querySelector('.custom-next');
+
+  if (swiper.isBeginning) {
+      prevButton.classList.add('inactive');
+  } else {
+      prevButton.classList.remove('inactive');
+  }
+
+  if (swiper.isEnd) {
+      nextButton.classList.add('inactive');
+  } else {
+      nextButton.classList.remove('inactive');
+  }
+}
